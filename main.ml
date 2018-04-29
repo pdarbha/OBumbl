fb132283cb69f06f76d2e9000442f39e6a072d1e
apoorvkh <apoorv.khand@gmail.com>
2018-04-29 15:41:18 -0400

    Finished terminal login/register

--- main.ml
+++ main.ml
@@ -2,30 +2,33 @@ open Profile
 
 type lr_variant = Login | Register
 
-let login_or_register =
-  let input = ref "" in
-  while !input <> "login" && !input <> "register" do
-    print_string "Enter \"login\" or \"register\": ";
-    input := String.lowercase_ascii (String.trim (read_line ()));
-    if !input == "login" then
-      (print_string "Enter username: ";
-      let username = String.trim (read_line ()) in
-      print_string "Enter password: ";
-      let password = String.trim (read_line ()) in
-      (Login, (Nethttp_client.Convenience.http_post "http://18.204.146.26/obumbl/login.php" [("username",username);("password",password)])))
-    else (if !input == "register" then
-      print_string "Enter username: ";
-      let username = String.trim (read_line ()) in
-      print_string "Enter password: ";
-      let password = String.trim (read_line ()) in
-      (Register, (Nethttp_client.Convenience.http_post "http://18.204.146.26/obumbl/login.php" [("username",username);("password",password)])))
-  done
+
 (* User either invokes login or register,
  * login: accepts username, password and looks up user id
  * register: input username, password, email, profile details (edit profile) -> push registration to server, return user id
  * max length of username is 16, max length of password is 256
  * returns (lr_variant, user id)
  *)
+let login_or_register =
+  let input = ref "" in
+  let output = ref (Login, "") in
+  let () = while !input <> "login" && !input <> "register" do
+    print_string "Enter \"login\" or \"register\": ";
+    input := String.lowercase_ascii (String.trim (read_line ()));
+    if !input = "login" then
+      (print_string "Enter username: ";
+      let username = String.trim (read_line ()) in
+      print_string "Enter password: ";
+      let password = String.trim (read_line ()) in
+      output := (Login, (Nethttp_client.Convenience.http_post "http://18.204.146.26/obumbl/login.php" [("username",username);("password",password)])))
+    else (if !input = "register" then
+      (print_string "Enter username: ";
+      let username = String.trim (read_line ()) in
+      print_string "Enter password: ";
+      let password = String.trim (read_line ()) in
+      output := (Register, (Nethttp_client.Convenience.http_post "http://18.204.146.26/obumbl/register.php" [("username",username);("password",password)]))))
+  done in
+  !output
 
 (* Pull all relevant data to user from server as json and store as record *)
 let pull_data id =
@@ -34,9 +37,11 @@ let pull_data id =
 (* *)
 let () =
   let lr = login_or_register in
-  let user_id = fst lr in
-  let user_profile : profile = Profile.lookup_profile user_id in
-  let data = pull_data user_id in
-  match snd lr with
-  | Login -> failwith "open invites"
-  | Register -> failwith "open tags"
+  let user_id = int_of_string (snd lr) in
+  if user_id = -1 then
+    failwith ((match fst lr with Login -> "Login" | Register -> "Register") ^ " unsuccessful.")
+  else  let user_profile : profile = Profile.lookup_profile user_id in
+    let data = pull_data user_id in
+    match fst lr with
+    | Login -> failwith "open tags"
+    | Register -> failwith "open edit profile"
