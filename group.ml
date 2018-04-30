5e77afaa7a8a5436c06019b160d517e16ea55d86
pdarbha <32006167+pdarbha@users.noreply.github.com>
2018-04-29 20:52:39 -0400

    minor fixes

--- group.ml
+++ group.ml
@@ -36,9 +36,12 @@ let rec create_group p =
   let purpose = print_read "Enter project code: " in
   let range_min = print_read "Enter minimum desired group size: " in
   let range_max = print_read "Enter maximum desired group size: " in
-  let params = [("user_id_list", string_of_int (user_id p));("purpose", purpose);("size","1");("range_min", range_min);("range_max", range_max);("group_blacklist","");("invited_groups_list","");("received_invites_list","")] in
-  let update = (Nethttp_client.Convenience.http_post "http://18.204.146.26/obumbl/insert_group.php" params) in
-    if update = "-1" then
-      (print_string "Group could not be created, try again.\n";
-      create_group p)
-    else (lookup_group (int_of_string update))
+  if (range_min>range_max ||range_max<0) then
+    (print_string "Make sure your max size is greater than 0 and greater than or equal to min size.\n"; create_group p)
+  else
+    let params = [("user_id_list", string_of_int (user_id p));("purpose", purpose);("size","1");("range_min", range_min);("range_max", range_max);("group_blacklist","");("invited_groups_list","");("received_invites_list","")] in
+    let update = (Nethttp_client.Convenience.http_post "http://18.204.146.26/obumbl/insert_group.php" params) in
+      if update = "-1" then
+        (print_string "Group could not be created, try again.\n";
+        create_group p)
+      else (lookup_group (int_of_string update))
