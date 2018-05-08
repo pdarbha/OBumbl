187ac036589d21b8ea121e6ff1491b06ea569607
Tal Moriah <talmoriah@dhcp-hol-4421.eduroam.cornell.edu>
2018-05-08 01:07:33 -0400

    Profile specs done

--- profile.ml
+++ profile.ml
@@ -17,18 +17,26 @@ open Nethttp_client.Convenience
 * Github URL - 60 char (URL encoded then decoded -- example: https://www.urlencoder.org)
 *)
 
-(* will store all information about each person including id, name, photo, description, etc.*)
+(* [type profile] stores all information about each person including id, name,
+   photo, description, etc. Record entries are immutable, and each entry must be
+   filled to complete a profile
+ * requires inputs from user to match declared types *)
 type profile = {user_id:int; name:string; photo:string ref; school:string;
                 group_id_list: int list; description: string;
                 interest_list: string list; experience : [ `BEG | `INT | `ADV ];
                 role: string; looking_for: ([ `BEG | `INT | `ADV ]*string) list;
                 github_url : string}
-
+(* [string_to_exp s] takes in a string s and matches that string to the
+   experience variant types.
+ * requires: a string*)
 let string_to_exp s =
   if s = "INT" then `INT
   else if s = "ADV" then `ADV
   else `BEG
 
+(* [string_to_looking_for s] takes in a string s and convers it to list form, to
+   represent the looking for category from profile.
+ * requires: a string*)
 let string_to_looking_for s =
   if s = "" then []
   else let lfList = split_string_to_list (String.sub s 0 ((String.length s)-1)) in
@@ -36,7 +44,9 @@ let string_to_looking_for s =
   List.map (fun el -> (string_to_exp (String.sub el 0 3),
                        String.sub el 4 ((String.length el)-4))) validLfList
 
-(* will take in Json file and parse it and store that information in an object of type profile *)
+(* [init_profile j] takes in Json file j and parses it, in order to store that
+   information in an object of type profile
+ * requires: a Json file*)
 let init_profile j =
   let id = j|>member "user_id"|>to_string|>int_of_string in
   let name = j|>member "name"|>to_string in
@@ -54,58 +64,95 @@ let init_profile j =
    description = desc; interest_list = interests; experience = exp; role = role;
    looking_for = look_for; github_url = github}
 
-
-(* will return the unique user id associated with a profile *)
+(* [user_id p] takes in a profile p and returns the unique user id associated
+   with that profile
+ * requires: a valid profile*)
 let user_id p = p.user_id
 
-(* will return the name of a user *)
+(* [name p] takes in a profile p and returns the name of the user associated
+   with that profile
+ * requires: a valid profile*)
 let name p = p.name
 
-(* will return the photo of a user, encoded as a Base64 string *)
+(* [photo p] takes in a profile p and returns the unique will return the photo
+   of the user associated with that profile, encoded as a Base64 string
+ * requires: a valid profile*)
 let photo p = !(p.photo)
 
-(* will return the school of a user *)
+(* [school p] takes in a profile p and returns the school of the user associated
+   with that profile
+ * requires: a valid profile*)
 let school p = p.school
 
-(* will return a list of a user’s tags *)
+(* [groups p] takes in a profile p and returns the groups of the user associated
+   with that profile
+ * requires: a valid profile*)
 let groups p = p.group_id_list
 
-(* will return a user’s description *)
+(* [description p] takes in a profile p and returns the description of the user
+   associated with that profile
+ * requires: a valid profile*)
 let description p = p.description
 
-(* will return a list of keyword interests for a user *)
+(* [interests p] takes in a profile p and returns a list of keyword interests of
+   the user associated with that profile
+ * requires: a valid profile*)
 let interests p = p.interest_list
 
-(* will return the experience of a user as a variant *)
+(* [experience p] takes in a profile p and returns the experience of the user
+   associated with that profile, as a variant
+ * requires: a valid profile*)
 let experience p = p.experience
 
-(* will return the role of a user *)
+(* [role p] takes in a profile p and returns the role of the user associated
+   with that profile
+ * requires: a valid profile*)
 let role p = p.role
 
-(* will return a tuple of experience as variant and role as string that user is looking for *)
+(* [looking_for p] takes in a profile p and returns a tuple of experience as
+   variant and role as string that the user associated with that profile is
+   looking for
+ * requires: a valid profile*)
 let looking_for p = p.looking_for
 
- (* will return a string to a github.com profile URL *)
+(* [github p] takes in a profile p and returns the string to a github.com
+   profile URL of the user associated with that profile
+ * requires: a valid profile*)
 let github p = p.github_url
 
+(* [exp_to_string e] takes in an experience e and matches it to its
+   corresponding string representation
+ * requires: a valid experience of type `BEG, `INT, or `ADV *)
 let exp_to_string e =
   match e with
   |`BEG -> "BEG"
   |`INT -> "INT"
   |`ADV -> "ADV"
 
+(* [exp_to_string e] takes in an experience e and matches it to its
+   corresponding full string representation
+ * requires: a valid experience of type `BEG, `INT, or `ADV *)
 let exp_print_string e =
   match e with
   |`BEG -> "Beginner"
   |`INT -> "Intermediate"
   |`ADV -> "Advanced"
 
+(* [looking_for_to_string lf] takes in a list lf and returns the string
+   representation of that list
+ * requires: a looking_for entry represented as a list *)
 let rec looking_for_to_string lf =
   match lf with
   | [] -> ""
   | h::t -> (exp_to_string (fst h)) ^ " " ^ (snd h) ^ ";" ^ (looking_for_to_string t)
 
-(* will accept a profile and two strings (field, value) to be updated and returned in new profile *)
+(* [edit p field new_val] takes in a profile p, a string field and a string
+   new_val. The entry corresponding to field will be updated to contain new_val,
+   and an updated profile will be returned
+ * requires: a valid profile, a valid field that corresponds to a field in
+   profile, and a new_value that is of the same type as the previous one
+ * raises failwith exception if field does not correpond to any of the record
+   fields of profile  *)
 let edit p field new_val =
   match field with
   |"user_id" -> (try let i = int_of_string new_val in {p with user_id = i}
@@ -126,15 +173,25 @@ let edit p field new_val =
   |"github_url" -> {p with github_url = new_val}
   | _ -> failwith "Must enter a valid field of profile to edit"
 
+(* [add_group p group_id] takes in a profile p and a group id group_id. It then
+   adds the new group's id to the user's group_id entry in profile and returns
+   an updated profile
+ * requires: a valid profile and group_id *)
 let add_group p group_id =
   edit p "group_id_list" (int_list_to_string (group_id::p.group_id_list))
 
+(* [remove_group p group_id] takes in a profile p and a group id group_id. It
+   then searches through the group_id entry to remove group_id from the user's
+   group list and returns an updated profile
+ * requires: a valid profile and group_id *)
 let remove_group p group_id =
   edit p "group_id_list"
     (int_list_to_string (List.filter (fun i -> i <> group_id) p.group_id_list))
 
-(* will take in a profile and uploads it to the server and returns true if it is uploaded
- * successfully. Has the side effect of changing information in the server. *)
+(* [update_server p] takes in a profile p and uploads it to the server. Returns
+   true if it has been uploaded successfully.
+ * requires: a valid profile
+ * side effects: changes information related to that profile in the server *)
 let update_server p =
   let params = [("user_id", string_of_int (p.user_id));("name", (p.name));
                 ("photo", (!(p.photo)));("school", (p.school));
@@ -147,6 +204,9 @@ let update_server p =
   let update = (http_post insert_prof_url params) in
   if update = "1" then true else false
 
+(* [cp_looking_for] looks for when the user is done entering looking for
+   entries, and connects them together in list form with the role and experience
+   the user wants *)
 let rec cp_looking_for ()=
   let lf_role = print_read ("Please enter a role you are looking for on your team"^
                            " or type \"done\": ") in
@@ -156,6 +216,10 @@ let rec cp_looking_for ()=
                                   " (INT), or advanced (ADV) " ^ lf_role ^ " ? ") in
   (string_to_exp lf_exp, lf_role)::(cp_looking_for ())
 
+(* [cp_interests] looks for when the user is done entering their interests, and
+   connects them together in list form. Tells the user to enter another interest
+   if their entry does not comply with the conditions for a valid interests
+   entry*)
 let rec cp_interests () =
   let interest = print_read "Please enter one of your interests or type \"done\": " in
   if String.lowercase_ascii (String.trim interest) = "done" || String.trim interest = ""
@@ -170,6 +234,10 @@ let rec cp_interests () =
       then interests_tail
     else interest::interests_tail
 
+(* [create_profile id] takes in an id. It prompts a user to enter all of the
+   profile fields, and if all entries are valid, it creates a profile for that
+   user which has the user_id id.
+ * requires: a valid user_id id *)
 let rec create_profile id =
   let n = print_read "\nEnter your full name: " in
   let s = print_read "Enter your school: " in
@@ -190,7 +258,11 @@ let rec create_profile id =
     (print_string "Invalid information, please enter your details again.\n";
     create_profile id)
 
-(* Query server and pull profile json from server, convert to profile type *)
+(* [lookup_profile id] takes in a user_id id and finds the profile associated
+   with that id. It queries the server and pulls that profile json from the
+   server, and converts it to a profile type
+ * requires: a valid user_id id corresponding to anexisting profile*)
+
 let rec lookup_profile id =
   let jProfileString = http_get (get_prof_url ^ (string_of_int id)) in
   if jProfileString = "-1"
